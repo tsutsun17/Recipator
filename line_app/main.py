8677e7d348bc691d7767403232017be8de49dbc3
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-07-14 22:26:58 +0900

    add current_node

--- line_app/main.py
+++ line_app/main.py
@@ -33,8 +33,6 @@ CHANNEL_SECRET = settings.CHANNEL_SECRET
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
-questions = tree.QuestionsClass()
-
 # set rich menu
 richmenu.createRichmenu(line_bot_api)
 
@@ -64,55 +62,59 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # TODO: ここでユーザーのstatusを確認
+    # users = db.session.query(User).\
+    #     filter(User.name=="name").\
+    #     all()
     answer_list = ["Yes", "No"]
     items = [QuickReplyButton(action=MessageAction(label=f"{answer}", text=f"{answer}")) for answer in answer_list]
-    global questions
 
     print(event)
 
     if event.message.text == 'Recipatorをはじめる':
-        questions = tree.QuestionsClass(status=1)
+        questions = tree.QuestionsClass()
         status, body = questions.call_first_question()
 
         messages = TextSendMessage(text=body, quick_reply=QuickReply(items=items))
         line_bot_api.reply_message(event.reply_token, messages=messages)
-    else:
-        # questionsのstatusが1出ない: 「Recipatorをはじめる」を押していない場合
+        return
+    
+    # questionsのstatusが1出ない: 「Recipatorをはじめる」を押していない場合
+    # TODO: statusで判断
+    if questions.status!=1:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='メニューから「Recipatorをはじめる」を押してみてね！')
+        )
+        return
 
-        if questions.status!=1:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='メニューから「Recipatorをはじめる」を押してみてね！')
-            )
-            return
+    # ボタンを押していない場合
+    if event.message.text!="Yes" and event.message.text!="No":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='ボタンを押して回答してね！')
+        )
+        questions.status = 1
+        return
 
-        # ボタンを押していない場合
-        if event.message.text!="Yes" and event.message.text!="No":
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='ボタンを押して回答してね！')
-            )
-            questions.status = 1
-            return
+    # 1: Yes, 0: No
+    ans = 1 if event.message.text == "Yes" else 0
 
-        # 1: Yes, 0: No
-        ans = 1 if event.message.text == "Yes" else 0
+    status, body = questions.cal_current_node(ans)
 
-        status, body = questions.cal_current_node(ans)
+    if status=='question':
+        messages = TextSendMessage(text=body, quick_reply=QuickReply(items=items))
+        line_bot_api.reply_message(event.reply_token, messages=messages)
+        questions.status = 1
+        return
 
-        if status=='question':
-            messages = TextSendMessage(text=body, quick_reply=QuickReply(items=items))
-            line_bot_api.reply_message(event.reply_token, messages=messages)
-            questions.status = 1
-            return
-
-        if status=='recipes':
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='//TODO: レシピ一覧')
-            )
-            questions.status = 0
-            return
+    if status=='recipes':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='//TODO: レシピ一覧')
+        )
+        questions.status = 0
+        return
 
     # line_bot_api.reply_message(
     #     event.reply_token,
@@ -121,6 +123,7 @@ def handle_message(event):
 
 @handler.add(FollowEvent)
 def handle_follow(event):
+    # TODO: ここでユーザー情報登録
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='フォローありがとう！ \nメニューから「Recipatorをはじめる」を押してみてね！')
--- line_app/models/user.py
+++ line_app/models/user.py
@@ -8,12 +8,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     status = db.Column(db.Integer, nullable=False)
     line_user_id = db.Column(db.String(255), index=True, nullable=False, unique=True)
+    curren_node = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
-    def __init__(self, status, line_user_id):
+    def __init__(self, status, line_user_id, current_node):
         self.status = status
         self.line_user_id = line_user_id
+        self.curren_node = current_node
 
     def to_dict(self):
         return {
